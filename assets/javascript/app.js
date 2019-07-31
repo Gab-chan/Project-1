17faf70826b269e9900f379572736f28a662c65b
Brent Haskins <brenthaskins@icloud.com>
2019-07-31 11:46:41 -0700

    Setting up HTML & JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBkQLTirJjsxhFrTzi6E2okJBJaJzs7UHY",
+    authDomain: "project-1-f809e.firebaseapp.com",
+    databaseURL: "https://project-1-f809e.firebaseio.com",
+    projectId: "project-1-f809e",
+    storageBucket: "",
+    messagingSenderId: "1000072791220",
+    appId: "1:1000072791220:web:afef53e16c957cea"
+  };
+
+
+firebase.initializeApp(config);
+
+// Create a variable to reference the database
+var database = firebase.database();
