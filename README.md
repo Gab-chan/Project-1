21860c97d378740a203498da42201e8c815be1f3
Brent Haskins <brenthaskins@icloud.com>
2019-07-31 19:33:27 -0700

    Updating files

--- README.md
+++ README.md
@@ -13,7 +13,8 @@
 
 
 ## APIs to be used
-
+Bestbuy
+Twitch
 
 ## Tasks
 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,6 +15,7 @@ firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 var userinput = "Destiny";
 
+
 // Here we are building the URL we need to query the database
 var queryURL = "https://api.bestbuy.com/v1/products((search=" + userinput + ")&(categoryPath.id=pcmcat300300050002))?apiKey=lig8iC6qhgDZu9pSlKu3IInC&format=json"
 
@@ -27,13 +28,36 @@ $.ajax({
 
       // Log the queryURL
       console.log(queryURL);
-
-      // Log the resulting object
       console.log(response);
 
+      var gameImg = $("<img>").attr("src", response.products[0].image);
+
       // Transfer content to HTML
-      $(".gamename").text("Game: " + JSON.stringify(response.products[0].name));
+      $(".gameimg").append(gameImg);
+      $(".gamename").text(JSON.stringify(response.products[0].albumTitle));
       $(".gamedesc").text(JSON.stringify(response.products[0].shortDescription));
+      $(".gameprice").text("Price: " + JSON.stringify(response.products[0].salePrice));
+
+      $(gameImg).width(160);
+      $(gameImg).height(140);
+
+
+      for (var i = 0; i < response.products.length; i++) {
+        $(".gamebox").clone().appendTo("body").text(response.products[i].albumTitle);
+          console.log(response.products[i].albumTitle);
+      }
+    
+
+
+    })
+
+
+
+
+
+
+          //   $(".gameimg").clone().appendTo(".gamebox2");
+    //   });
 
 
     //   for (var i = 0; i < response.products.length; i++) {
@@ -41,4 +65,10 @@ $.ajax({
     //   console.log(response.products[i].name)
         
     //   }
-    });
+
+
+
+//     for (var i = 0; i < response.products.length; i++) {
+//     $("").append(JSON.stringify(response.products[0].albumTitle));
+// console.log(response.products[i].albumTitle)
+
