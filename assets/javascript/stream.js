0491a8f700ba15baeeed7dc5576530caa694d3b5
NelsonCad <NelsonCad97@gmail.com>
2019-08-02 11:04:48 -0700

    started the logic for getting the streams

--- assets/javascript/stream.js
+++ assets/javascript/stream.js
@@ -1,17 +1,48 @@
 $(document).ready(function () {
 
-    let queryURL = 'https://cors-anywhere.herokuapp.com/https://api.twitch.tv/kraken/search/streams?query=destiny%202';
+    var config = {
+        apiKey: "AIzaSyBkQLTirJjsxhFrTzi6E2okJBJaJzs7UHY",
+        authDomain: "project-1-f809e.firebaseapp.com",
+        databaseURL: "https://project-1-f809e.firebaseio.com",
+        projectId: "project-1-f809e",
+        storageBucket: "",
+        messagingSenderId: "1000072791220",
+        appId: "1:1000072791220:web:afef53e16c957cea"
+    };
+
+
+    firebase.initializeApp(config);
+
+    // Create a variable to reference the database
+    var database = firebase.database();
+
+    let hunted = "";
+
+    $("#game-search-btn").on("click", function () {
+
+        hunted = $("game-search").val().trim();
+
+        database.ref().set({ searchedGame: hunted })
+
+        $("game-search").val("");
+    });
 
     function RadaRada() {
+
+        let queryURL = 'https://cors-anywhere.herokuapp.com/https://api.twitch.tv/kraken/search/streams?query=' + hunted;
         $.ajax({
             url: queryURL,
-            headers: {"Client-ID": "9b0njddb6vtmna80nklh2bna9y52v7"},
+            headers: { "Client-ID": "9b0njddb6vtmna80nklh2bna9y52v7" },
             method: "GET"
-        }).then(function (response) {          
+        }).then(function (response) {
             console.log(response);
         });
-    }
+    };
 
-    RadaRada();
+    database.ref().on("value", function (snap) {
 
+        hunted = snap.val();
+
+        RadaRada();
+    });
 });
