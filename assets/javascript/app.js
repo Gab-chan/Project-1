b2c3c7ffc65199d9eb5b85c062fab238bde76526
NelsonCad <NelsonCad97@gmail.com>
2019-08-06 11:05:33 -0700

    fixed some format bugs on computer screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     $(".gamelistbox").hide();
     $(".bscontainer").show();
-    
+
 });
 
 
@@ -37,7 +37,7 @@ $(".submit").on("click", function (event) {
     $(".gamelistbox").show();
     $(".bscontainer").hide();
 
-    if( $("input:text").val().length === 0 ) {
+    if ($("input:text").val().length === 0) {
         $(".bscontainer").show();
         $(".gamelistbox").hide();
         $(".searchbar").addClass("warning");
@@ -46,7 +46,7 @@ $(".submit").on("click", function (event) {
         setTimeout(function () {
             $(".searchbar").removeClass("warning");
         }, 2500);
-     }
+    }
 
     // $("input:text").text(function () { $(this).val("") });
 
@@ -67,79 +67,79 @@ $(".submit").on("click", function (event) {
             console.log(queryURL);
             console.log(response);
 
-                var gameImg = $("<img>").attr("src", response.products[0].image);
+            var gameImg = $("<img>").attr("src", response.products[0].image);
 
-                $(".gameimg").html(gameImg);
-                $(".gamename").html(response.products[0].albumTitle)
-                $(".gamedesc").html(response.products[0].longDescription);
-                $(".gameprice").html("Price: " + response.products[0].salePrice + " -");
-                $(".buynow").appendTo(".gameprice");
+            $(".gameimg").html(gameImg);
+            $(".gamename").html(response.products[0].albumTitle)
+            $(".gamedesc").html(response.products[0].longDescription);
+            $(".gameprice").html("Price: " + response.products[0].salePrice + " -");
+            $(".buynow").appendTo(".gameprice");
 
-                $(gameImg).attr("height", "400px");
-                $(gameImg).attr("width", "400px");
-
-
-                database.ref().child("searched-games").push({
-                    gamesearch: gamesearch
-                  });
-
-                })
-        });
-
-        var queryURL = "https://api.bestbuy.com/v1/products((search=Games)&(categoryPath.id=pcmcat300300050002))?apiKey=lig8iC6qhgDZu9pSlKu3IInC&sort=bestSellingRank.asc&show=name,bestSellingRank,image,addToCartUrl&pageSize=5&format=json";
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-    
-    
-            // We store all of the retrieved data inside of an object called "response"
-            .then(function (response) {
-    
-    
-                // Log the queryURL
-                console.log(queryURL);
-                console.log(response);
-
-                    var best1 = $("<img>").attr("src", response.products[0].image);
-                    var best2 = $("<img>").attr("src", response.products[1].image);
-                    var best3 = $("<img>").attr("src", response.products[2].image);
-                    var best4 = $("<img>").attr("src", response.products[3].image);
-                    var best5 = $("<img>").attr("src", response.products[4].image);
-
-                    $(".topimg1").append(best1);
-                    $(".topname1").append(response.products[0].name);
-                    $(".topimg2").append(best2);
-                    $(".topname2").append(response.products[1].name);
-                    $(".topimg3").append(best3);
-                    $(".topname3").append(response.products[2].name);
-                    $(".topimg4").append(best4);
-                    $(".topname4").append(response.products[3].name);
-                    $(".topimg5").append(best5);
-                    $(".topname5").append(response.products[4].name);
-
-                    $(best1).attr("height", "175px");
-                    $(best1).attr("width", "175px");
-                    $(best2).attr("height", "175px");
-                    $(best2).attr("width", "175px");
-                    $(best3).attr("height", "175px");
-                    $(best3).attr("width", "175px");
-                    $(best4).attr("height", "175px");
-                    $(best4).attr("width", "175px");
-                    $(best5).attr("height", "175px");
-                    $(best5).attr("width", "175px");
+            $(gameImg).attr("height", "400px");
+            $(gameImg).attr("width", "400px");
 
 
-
+            database.ref().child("searched-games").push({
+                gamesearch: gamesearch
             });
+
+        })
+});
+
+var queryURL = "https://api.bestbuy.com/v1/products((search=Games)&(categoryPath.id=pcmcat300300050002))?apiKey=lig8iC6qhgDZu9pSlKu3IInC&sort=bestSellingRank.asc&show=name,bestSellingRank,image,addToCartUrl&pageSize=5&format=json";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
+
+
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function (response) {
+
+
+        // Log the queryURL
+        console.log(queryURL);
+        console.log(response);
+
+        var best1 = $("<img>").attr("src", response.products[0].image);
+        var best2 = $("<img>").attr("src", response.products[1].image);
+        var best3 = $("<img>").attr("src", response.products[2].image);
+        var best4 = $("<img>").attr("src", response.products[3].image);
+        var best5 = $("<img>").attr("src", response.products[4].image);
+
+        $(".topimg1").append(best1);
+        $(".topname1").append(response.products[0].name);
+        $(".topimg2").append(best2);
+        $(".topname2").append(response.products[1].name);
+        $(".topimg3").append(best3);
+        $(".topname3").append(response.products[2].name);
+        $(".topimg4").append(best4);
+        $(".topname4").append(response.products[3].name);
+        $(".topimg5").append(best5);
+        $(".topname5").append(response.products[4].name);
+
+        $(best1).attr("height", "175px");
+        $(best1).attr("width", "175px");
+        $(best2).attr("height", "175px");
+        $(best2).attr("width", "175px");
+        $(best3).attr("height", "175px");
+        $(best3).attr("width", "175px");
+        $(best4).attr("height", "175px");
+        $(best4).attr("width", "175px");
+        $(best5).attr("height", "175px");
+        $(best5).attr("width", "175px");
+
+
+
+    });
 
 
 
             // var cloned = $('.gamelistbox').first().clone();
             // cloned.insertAfter(".gamelistbox");
 
-        
+
 
 
             // for (var i = 0; i < response.products.length; i++) {
